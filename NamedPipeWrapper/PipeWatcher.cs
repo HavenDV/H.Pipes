26b9aa6f148a75736efc4f0843f23617475b7cc5
Konstantin S <havendv@gmail.com>
2020-01-06 07:35:15 +1000

    Added PipeWatcher

---
+++ NamedPipeWrapper/PipeWatcher.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using NamedPipeWrapper.Args;
+
+// ReSharper disable UnusedMember.Global
+
+namespace NamedPipeWrapper
+{
+    /// <summary>
+    /// Watches the directory "\\.\pipe\" and reports on new events
+    /// </summary>
+    public sealed class PipeWatcher : IDisposable
+    {
+        #region Properties
+        
+        /// <summary>
+        /// Returns <see langword="true"/> if <see cref="PipeWatcher"/> is active
+        /// </summary>
+        public bool IsStarted => Timer.Enabled;
+
+        private Timer Timer { get; }
+
+        private List<string> LastPipes { get; set; } = new List<string>();
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// When any pipe created
+        /// </summary>
+        public event EventHandler<NameEventArgs>? Created;
+
+        /// <summary>
+        /// When any pipe deleted
+        /// </summary>
+        public event EventHandler<NameEventArgs>? Deleted;
+
+        private void OnCreated(string name)
+        {
+            Created?.Invoke(this, new NameEventArgs(name));
+        }
+
+        private void OnDeleted(string name)
+        {
+            Deleted?.Invoke(this, new NameEventArgs(name));
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Create new instance of watcher <br/>
+        /// Default interval is <see langword="100 milliseconds"/>
+        /// </summary>
+        /// <param name="interval"></param>
+        public PipeWatcher(TimeSpan? interval = default)
+        {
+            Timer = new Timer((interval ?? TimeSpan.FromMilliseconds(100)).TotalMilliseconds);
+            Timer.Elapsed += OnElapsed;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void OnElapsed(object sender, ElapsedEventArgs args)
+        {
+            var pipes = GetActivePipes();
+
+            foreach (var name in pipes.Except(LastPipes))
+            {
+                OnCreated(name);
+            }
+            foreach (var name in LastPipes.Except(pipes))
+            {
+                OnDeleted(name);
+            }
+
+            LastPipes = pipes;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Starts watching
+        /// </summary>
+        public void Start()
+        {
+            LastPipes = GetActivePipes();
+            Timer.Start();
+        }
+
+        /// <summary>
+        /// Stops watching(without disposing) <br/>
+        /// You can call <see cref="Start()"/> again if it is required
+        /// </summary>
+        public void Stop()
+        {
+            Timer.Stop();
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        /// <summary>
+        /// Dispose internal <see cref="Timer"/>
+        /// </summary>
+        public void Dispose()
+        {
+            Timer.Dispose();
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Checks if a given pipe name exists
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsExists(string name)
+        {
+            return GetActivePipes().Contains(name);
+        }
+
+        /// <summary>
+        /// Returns list of active pipes
+        /// </summary>
+        public static List<string> GetActivePipes()
+        {
+            return Directory
+                .GetFiles(@"\\.\pipe\")
+                .Select(path => path.Replace(@"\\.\pipe\", string.Empty))
+                .ToList();
+        }
+
+        /// <summary>
+        /// Create new instance of watcher and start it<br/>
+        /// Default interval is <see langword="100 milliseconds"/>
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public static PipeWatcher CreateAndStart(TimeSpan? interval = default)
+        {
+            var watcher = new PipeWatcher(interval);
+            watcher.Start();
+
+            return watcher;
+        }
+
+        #endregion
+
+    }
+}
