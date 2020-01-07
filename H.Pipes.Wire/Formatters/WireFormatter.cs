fdc453d0577c4018a1cb5490ac425d8f5452f15a
Konstantin S <havendv@gmail.com>
2020-01-08 08:48:26 +1000

    Added WireFormatter project

---
+++ H.Pipes.Wire/Formatters/WireFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NamedPipeWrapper.Formatters;
+using Wire;
+
+namespace H.Pipes.Formatters
+{
+    public class WireFormatter : IFormatter
+    {
+        private Serializer Serializer { get; } = new Serializer();
+
+        public byte[] Serialize(object obj)
+        {
+            using var stream = new MemoryStream();
+            Serializer.Serialize(obj, stream);
+
+            return stream.ToArray();
+        }
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            using var memoryStream = new MemoryStream(bytes);
+
+            return (T)Serializer.Deserialize(memoryStream);
+        }
+    }
+}
---
+++ H.Pipes.Wire/WirePipeClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using H.Pipes.Formatters;
+using NamedPipeWrapper;
+
+namespace H.Pipes
+{
+    /// <summary>
+    /// Wraps a <see cref="NamedPipeClient{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Reference type to read/write from the named pipe</typeparam>
+    public class WirePipeClient<T> : NamedPipeClient<T>
+        where T : class
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a new <see cref="WirePipeClient{T}"/> to connect to the <see cref="WirePipeClient{T}"/> specified by <paramref name="pipeName"/>. <br/>
+        /// Default reconnection interval - <see langword="100 ms"/>
+        /// </summary>
+        /// <param name="pipeName">Name of the server's pipe</param>
+        /// <param name="serverName">the Name of the server, default is  local machine</param>
+        /// <param name="reconnectionInterval">Default reconnection interval - <see langword="100 ms"/></param>
+        public WirePipeClient(string pipeName, string serverName = ".", TimeSpan? reconnectionInterval = default) : base(pipeName, serverName, reconnectionInterval, new WireFormatter())
+        {
+        }
+
+        #endregion
+    }
+}
