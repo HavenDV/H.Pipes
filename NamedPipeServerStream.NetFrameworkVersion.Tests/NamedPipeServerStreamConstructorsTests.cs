8340e99c8c50ac319d8955a08fe8aa932cac08b8
Konstantin S <havendv@gmail.com>
2020-01-20 10:30:31 +1000

    Added NamedPipeServerStream.NetFrameworkVersion tests

---
+++ NamedPipeServerStream.NetFrameworkVersion.Tests/NamedPipeServerStreamConstructorsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.IO.Pipes.Tests
+{
+    [TestClass]
+    public class NamedPipeServerStreamConstructorsTests
+    {
+        [TestMethod]
+        public void ExceptionTest()
+        {
+            var exception = Assert.ThrowsException<IOException>(() =>
+            {
+                using var stream1 = NamedPipeServerStreamConstructors.New(nameof(NamedPipeServerStreamConstructorsTests));
+                using var stream2 = NamedPipeServerStreamConstructors.New(nameof(NamedPipeServerStreamConstructorsTests));
+            });
+
+            Console.WriteLine(exception.ToString());
+        }
+
+        [TestMethod]
+        public void DisposeTest()
+        {
+            {
+                using var stream = NamedPipeServerStreamConstructors.New(nameof(NamedPipeServerStreamConstructorsTests));
+            }
+            {
+                using var stream = NamedPipeServerStreamConstructors.New(nameof(NamedPipeServerStreamConstructorsTests));
+            }
+        }
+    }
+}
