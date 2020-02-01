6c10e6f60688869d4a47359092b536f9b5b6dbd2
Konstantin S <havendv@gmail.com>
2020-02-02 08:45:29 +1000

    Added new tests

--- H.Pipes.Tests/SingleConnectionPipeClientTests.cs
+++ H.Pipes.Tests/SingleConnectionPipeClientTests.cs
@@ -9,12 +9,40 @@ namespace H.Pipes.Tests
     public class SingleConnectionPipeClientTests
     {
         [TestMethod]
-        public async Task ClientConnectTest()
+        public async Task ClientConnectCancellationTest()
         {
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(1));
             await using var client = new SingleConnectionPipeClient<string>("this_pipe_100%_is_not_exists");
 
             await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await client.ConnectAsync(cancellationTokenSource.Token));
+        }
+
+        [TestMethod]
+        public async Task ClientConnectTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            await using var client = new SingleConnectionPipeClient<string>(nameof(ClientConnectTest));
+            await using var server = new SingleConnectionPipeServer<string>(nameof(ClientConnectTest));
+
+            await server.StartAsync(cancellationToken: cancellationTokenSource.Token);
+
+            await client.ConnectAsync(cancellationTokenSource.Token);
+        }
+
+        [TestMethod]
+        public async Task ClientDoubleConnectTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            await using var client = new SingleConnectionPipeClient<string>(nameof(ClientDoubleConnectTest));
+            await using var server = new SingleConnectionPipeServer<string>(nameof(ClientDoubleConnectTest));
+
+            await server.StartAsync(cancellationToken: cancellationTokenSource.Token);
+
+            await client.ConnectAsync(cancellationTokenSource.Token);
+
+            await client.DisconnectAsync(cancellationTokenSource.Token);
+
+            await client.ConnectAsync(cancellationTokenSource.Token);
         }
     }
 }
