18dd819456b8945042ae7ef56c4cef1bcc85d65d
Konstantin S <HavenDV@users.noreply.github.com>
2020-10-14 02:01:55 +1000

    Added Net Framework constructor for net targets.

--- H.Pipes.AccessControl/PipeServerExtensions.cs
+++ H.Pipes.AccessControl/PipeServerExtensions.cs
@@ -21,7 +21,11 @@ namespace H.Pipes.AccessControl
             server = server ?? throw new ArgumentNullException(nameof(server));
             pipeSecurity = pipeSecurity ?? throw new ArgumentNullException(nameof(pipeSecurity));
 
+#if NET45 || NET46
+            server.CreatePipeStreamFunc = pipeName => new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous | PipeOptions.WriteThrough, 0, 0, pipeSecurity);
+#else
             server.CreatePipeStreamFunc = pipeName => NamedPipeServerStreamConstructors.New(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous | PipeOptions.WriteThrough, 0, 0, pipeSecurity);
+#endif
         }
 
         /// <summary>
