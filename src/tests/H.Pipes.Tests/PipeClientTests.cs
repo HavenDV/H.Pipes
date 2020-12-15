869add269a53b9fec4644249049e7b484134724c
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-15 15:56:51 +1000

    Added test for issue [#3].

--- src/tests/H.Pipes.Tests/PipeClientTests.cs
+++ src/tests/H.Pipes.Tests/PipeClientTests.cs
@@ -14,7 +14,37 @@ namespace H.Pipes.Tests
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(1));
             await using var client = new PipeClient<string>("this_pipe_100%_is_not_exists");
 
-            await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await client.ConnectAsync(cancellationTokenSource.Token));
+            await Assert.ThrowsExceptionAsync<OperationCanceledException>(
+                async () => await client.ConnectAsync(cancellationTokenSource.Token));
+        }
+        
+        [TestMethod]
+        public async Task WriteAsyncTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(15));
+            var cancellationToken = cancellationTokenSource.Token;
+            
+            await using var client = new PipeClient<string>("this_pipe_100%_is_not_exists");
+
+            var firstTask = Task.Run(async () =>
+            {
+                using var source = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                source.CancelAfter(TimeSpan.FromSeconds(1));
+
+                // ReSharper disable once AccessToDisposedClosure
+                await client.WriteAsync(string.Empty, source.Token);
+            }, cancellationToken);
+
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+            {
+                using var source = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                source.CancelAfter(TimeSpan.FromSeconds(1));
+                
+                await client.WriteAsync(string.Empty, source.Token);
+            }
+
+            await firstTask;
         }
     }
 }
