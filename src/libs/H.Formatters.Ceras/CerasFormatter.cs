69cd8d6670278f4d1292288f5bbc41149e3ec52e
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-25 03:29:12 +1000

    feat: Added CerasFormatter package.

---
+++ src/libs/H.Formatters.Ceras/CerasFormatter.cs
@@ -0,0 +1,49 @@
+﻿using Ceras;
+using H.Formatters.Utilities;
+
+namespace H.Formatters;
+
+/// <summary>
+/// A formatter that uses <see cref="CerasSerializer"/> inside for serialization/deserialization
+/// </summary>
+public class CerasFormatter : IFormatter
+{
+    private CerasSerializer InternalFormatter { get; } = new CerasSerializer();
+
+    /// <summary>
+    /// Serializes using <see cref="CerasSerializer"/>
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<byte[]> SerializeAsync(object? obj, CancellationToken cancellationToken = default)
+    {
+        if (obj == null)
+        {
+            return TaskUtilities.FromResult(ArrayUtilities.Empty<byte>());
+        }
+
+        var bytes = InternalFormatter.Serialize(obj);
+
+        return TaskUtilities.FromResult(bytes);
+    }
+
+    /// <summary>
+    /// Deserializes using <see cref="CerasSerializer"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="bytes"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<T?> DeserializeAsync<T>(byte[]? bytes, CancellationToken cancellationToken = default)
+    {
+        if (bytes == null || !bytes.Any())
+        {
+            return TaskUtilities.FromResult<T?>(default);
+        }
+
+        var obj = InternalFormatter.Deserialize<T?>(bytes);
+
+        return TaskUtilities.FromResult(obj);
+    }
+}
