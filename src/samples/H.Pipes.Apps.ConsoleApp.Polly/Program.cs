3894e516e63e3f97810a918f562dd44e8f818bde
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-30 08:25:46 +1000

    example: Added Polly usage example.

--- src/samples/H.Pipes.Apps.ConsoleApp.Polly/Program.cs
+++ src/samples/H.Pipes.Apps.ConsoleApp.Polly/Program.cs
@@ -1,18 +1,14 @@
 ﻿using H.Pipes;
+using Polly;
 
 const string DefaultPipeName = "named_pipe_test_server";
 
-string? mode;
+string mode = string.Empty;
 string pipeName = DefaultPipeName;
 if (args.Any())
 {
     mode = args.ElementAt(0);
     pipeName = args.ElementAtOrDefault(1) ?? DefaultPipeName;
-}
-else
-{
-    Console.WriteLine("Enter mode('server' or 'client'):");
-    mode = await Console.In.ReadLineAsync().ConfigureAwait(false);
 }
 
 static void OnExceptionOccurred(Exception exception)
@@ -61,16 +57,23 @@ try
                     {
                         try
                         {
-                            var message = await Console.In.ReadLineAsync().ConfigureAwait(false);
-                            if (message == "q")
+                            await Task.Delay(TimeSpan.FromSeconds(1));
+
+                            if (new Random().Next(0, 10) == 0)
                             {
-                                source.Cancel();
-                                break;
+                                await server.StopAsync();
+                                Console.WriteLine($"Server is stopped");
+                            }
+                            else if (!server.IsStarted && new Random().Next(0, 10) == 0)
+                            {
+                                await server.StartAsync();
+                                Console.WriteLine($"Server is started");
                             }
 
-                            Console.WriteLine($"Sent to {server.ConnectedClients.Count} clients");
-
-                            await server.WriteAsync(message ?? string.Empty, source.Token).ConfigureAwait(false);
+                            if (server.IsStarted)
+                            {
+                                await server.WriteAsync("Test message", source.Token).ConfigureAwait(false);
+                            }
                         }
                         catch (Exception exception)
                         {
@@ -94,13 +97,24 @@ try
                 Console.WriteLine($"Running in CLIENT mode. PipeName: {pipeName}");
                 Console.WriteLine("Enter 'q' to exit");
 
-                await using var client = new PipeClient<string>(pipeName);
+                await using var client = new PipeClient<string>(pipeName)
+                {
+                    AutoReconnect = false,
+                };
                 client.MessageReceived += (o, args) => Console.WriteLine("MessageReceived: " + args.Message);
                 client.Disconnected += (o, args) => Console.WriteLine("Disconnected from server");
                 client.Connected += (o, args) => Console.WriteLine("Connected to server");
                 client.ExceptionOccurred += (o, args) => OnExceptionOccurred(args.Exception);
 
-                // Dispose is not required
+                var policy = Policy.WrapAsync(
+                    Policy
+                        .Handle<InvalidOperationException>()
+                        .RetryAsync(3, async (exception, count) =>
+                        {
+                            await client.ConnectAsync();
+                        }),
+                    Policy.TimeoutAsync(TimeSpan.FromSeconds(1)));
+
                 var _ = Task.Run(async () =>
                 {
                     while (true)
@@ -114,7 +128,8 @@ try
                                 break;
                             }
 
-                            await client.WriteAsync(message ?? string.Empty, source.Token).ConfigureAwait(false);
+                            await policy.ExecuteAsync(async cancellationToken =>
+                                await client.WriteAsync(message ?? string.Empty, cancellationToken), source.Token).ConfigureAwait(false);
                         }
                         catch (Exception exception)
                         {
