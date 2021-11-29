b97a0a210f63b8685a8597904c706a37ce4b1045
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-30 06:04:16 +1000

    example: Added ConsoleApp.Polly template project.

---
+++ src/samples/H.Pipes.Apps.ConsoleApp.Polly/Program.cs
@@ -0,0 +1,141 @@
+﻿using H.Pipes;
+
+const string DefaultPipeName = "named_pipe_test_server";
+
+string? mode;
+string pipeName = DefaultPipeName;
+if (args.Any())
+{
+    mode = args.ElementAt(0);
+    pipeName = args.ElementAtOrDefault(1) ?? DefaultPipeName;
+}
+else
+{
+    Console.WriteLine("Enter mode('server' or 'client'):");
+    mode = await Console.In.ReadLineAsync().ConfigureAwait(false);
+}
+
+static void OnExceptionOccurred(Exception exception)
+{
+    Console.Error.WriteLine($"Exception: {exception}");
+}
+
+try
+{
+    using var source = new CancellationTokenSource();
+
+    switch (mode?.ToUpperInvariant())
+    {
+        case "SERVER":
+            {
+                Console.WriteLine($"Running in SERVER mode. PipeName: {pipeName}");
+                Console.WriteLine("Enter 'q' to exit");
+
+                await using var server = new PipeServer<string>(pipeName);
+                server.ClientConnected += async (_, args) =>
+                {
+                    Console.WriteLine($"Client {args.Connection.Id} is now connected!");
+
+                    try
+                    {
+                        await args.Connection.WriteAsync("Welcome!", source.Token).ConfigureAwait(false);
+                    }
+                    catch (Exception exception)
+                    {
+                        OnExceptionOccurred(exception);
+                    }
+                };
+                server.ClientDisconnected += (_, args) =>
+                {
+                    Console.WriteLine($"Client {args.Connection.Id} disconnected");
+                };
+                server.MessageReceived += (_, args) =>
+                {
+                    Console.WriteLine($"Client {args.Connection.Id} says: {args.Message}");
+                };
+                server.ExceptionOccurred += (_, args) => OnExceptionOccurred(args.Exception);
+
+                var _ = Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var message = await Console.In.ReadLineAsync().ConfigureAwait(false);
+                            if (message == "q")
+                            {
+                                source.Cancel();
+                                break;
+                            }
+
+                            Console.WriteLine($"Sent to {server.ConnectedClients.Count} clients");
+
+                            await server.WriteAsync(message ?? string.Empty, source.Token).ConfigureAwait(false);
+                        }
+                        catch (Exception exception)
+                        {
+                            OnExceptionOccurred(exception);
+                        }
+                    }
+                }, source.Token);
+
+                Console.WriteLine("Server starting...");
+
+                await server.StartAsync(cancellationToken: source.Token).ConfigureAwait(false);
+
+                Console.WriteLine("Server is started!");
+
+                await Task.Delay(Timeout.InfiniteTimeSpan, source.Token).ConfigureAwait(false);
+                break;
+            }
+
+        default:
+            {
+                Console.WriteLine($"Running in CLIENT mode. PipeName: {pipeName}");
+                Console.WriteLine("Enter 'q' to exit");
+
+                await using var client = new PipeClient<string>(pipeName);
+                client.MessageReceived += (o, args) => Console.WriteLine("MessageReceived: " + args.Message);
+                client.Disconnected += (o, args) => Console.WriteLine("Disconnected from server");
+                client.Connected += (o, args) => Console.WriteLine("Connected to server");
+                client.ExceptionOccurred += (o, args) => OnExceptionOccurred(args.Exception);
+
+                // Dispose is not required
+                var _ = Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var message = await Console.In.ReadLineAsync().ConfigureAwait(false);
+                            if (message == "q")
+                            {
+                                source.Cancel();
+                                break;
+                            }
+
+                            await client.WriteAsync(message ?? string.Empty, source.Token).ConfigureAwait(false);
+                        }
+                        catch (Exception exception)
+                        {
+                            OnExceptionOccurred(exception);
+                        }
+                    }
+                }, source.Token);
+
+                Console.WriteLine("Client connecting...");
+
+                await client.ConnectAsync(source.Token).ConfigureAwait(false);
+
+                await Task.Delay(Timeout.InfiniteTimeSpan, source.Token).ConfigureAwait(false);
+                break;
+            }
+    }
+}
+catch (OperationCanceledException)
+{
+}
+catch (Exception exception)
+{
+    OnExceptionOccurred(exception);
+}
