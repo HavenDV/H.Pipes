64598ad335b92687e5be7408d116ba11bb35fdc9
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-12-20 22:22:38 +1000

    feat: Added Inferno PipeConnectionExtensions.WaitExchangeAsync extension.

---
+++ src/libs/H.Formatters.Inferno/PipeConnectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using H.Pipes;
+
+namespace H.Formatters;
+
+/// <summary>
+/// Encryption <see cref="PipeConnection{T}"/> extensions.
+/// </summary>
+public static class PipeConnectionExtensions
+{
+    /// <summary>
+    /// Waits key exchange.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="connection"></param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static async void WaitExchangeAsync<T>(
+        this PipeConnection<T> connection,
+        CancellationToken cancellationToken = default)
+    {
+        connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+        while (connection.Formatter is not InfernoFormatter)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(1), cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
