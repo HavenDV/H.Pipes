a34a0a2e08c129b449c28454a9102b5153a07277
HavenDV <havendv@gmail.com>
2024-01-20 17:08:50 +0400

    feat: Added MessagePackFormatter<T1> to H.Formatters.MessagePack lib.

---
+++ src/libs/H.Formatters.MessagePack/MessagePackFormatter`1.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace H.Formatters;
+
+/// <summary>
+/// A formatter that uses <see cref="MessagePackSerializer"/> inside for serialization/deserialization. <br/>
+/// This formatter is needed to avoid the following error: <br/>
+/// https://github.com/HavenDV/H.Pipes/issues/48
+/// </summary>
+public class MessagePackFormatter<T1> : FormatterBase
+{
+    protected override byte[] SerializeInternal(object obj)
+    {
+        return MessagePackSerializer.Serialize<T1>((T1)obj);
+    }
+
+    protected override T DeserializeInternal<T>(byte[] bytes)
+    {
+        return MessagePackSerializer.Deserialize<T>(bytes);
+    }
+}
